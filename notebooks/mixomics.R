eb5c01f78cb0375a6494fdf071e495e7d25069d6
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-08-11 15:39:38 +0800

    Add comments

--- notebooks/mixomics.R
+++ notebooks/mixomics.R
@@ -4,6 +4,8 @@ library(stringr)
 
 df = read.csv("data/ampliseq/ampliseq_results/qiime2/abundance_tables/abs-abund-table-4.tsv",
                        sep="\t", skip = 1)
+
+# Concatenate unknowns and format row names
 fmt_rows = str_split_fixed(df$X.OTU.ID, ";", n=4)[,4]
 fmt_rows = replace(fmt_rows, fmt_rows=="", "Unknown")
 df$X.OTU.ID = fmt_rows
@@ -11,11 +13,14 @@ df = df %>% group_by(X.OTU.ID) %>% summarise(across(everything(), ~ sum(.))) %>%
 row.names(df) = df$X.OTU.ID
 df = df[,-1] %>% t
 
+
+# Sparse PCA
 model = spca(df+1, logratio="CLR", center=T, scale=T)
 plotIndiv(model)
 plotVar(model)
 cim(model)
 
+# PCA
 model = pca(df+1, logratio="CLR", center=T, scale=T)
 plotIndiv(model)
 plotVar(model)
